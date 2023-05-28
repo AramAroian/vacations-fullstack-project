5490dd0854ea415fbd68e0238ff9db6472cd7d00
AramAroian <aramaroian1@gmail.com>
2023-05-28 17:06:53 +0300

    updated database

--- Database/vacations.sql
+++ Database/vacations.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2023 at 03:06 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: May 28, 2023 at 04:06 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,42 @@ CREATE TABLE `followers` (
   `vacationsId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `followers`
+--
+
+INSERT INTO `followers` (`usersId`, `vacationsId`) VALUES
+(1, 4),
+(3, 9),
+(2, 4),
+(3, 8),
+(3, 7),
+(3, 11),
+(3, 10),
+(3, 5),
+(5, 11),
+(5, 7),
+(5, 4),
+(1, 10),
+(1, 8),
+(1, 5),
+(1, 7),
+(1, 6),
+(1, 12),
+(1, 9),
+(1, 1),
+(1, 2),
+(5, 6),
+(5, 3),
+(5, 8),
+(5, 10),
+(11, 12),
+(11, 10),
+(11, 8),
+(11, 3),
+(5, 12),
+(5, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -57,7 +93,10 @@ INSERT INTO `users` (`usersId`, `firstName`, `lastName`, `email`, `password`, `a
 (1, 'Ding', 'Liren', 'dingliren@gmail.com', 'Dl123456', 'user'),
 (2, 'Richard', 'Rapport', 'richardrapport@gmail.com', 'Rr123456', 'user'),
 (3, 'Hikaru', 'Nakamura', 'hikarunakamura@gmail.com', 'Hn123456', 'user'),
-(4, 'Admin', 'Adminovich', 'admin@gmail.com', 'Aa123456', 'admin');
+(4, 'Admin', 'Adminovich', 'admin@gmail.com', 'Aa123456', 'admin'),
+(5, 'Hans', 'Niemann', 'hansniemann@gmail.com', 'HN123456', 'user'),
+(10, 'Hans', 'Niemann', 'hansniemann@gmail.com', 'HN123456', 'user'),
+(11, 'Fabiano', 'Caruana', 'fabianocaruana@gmail.com', 'Fc123456', 'user');
 
 -- --------------------------------------------------------
 
@@ -80,19 +119,18 @@ CREATE TABLE `vacations` (
 --
 
 INSERT INTO `vacations` (`vacationsId`, `destination`, `description`, `startDate`, `endDate`, `price`, `imageName`) VALUES
-(1, 'Budapest', 'Experience the beauty of Budapest, known as the \"Paris of the East.\" Admire the stunning architecture, soak in the thermal baths, and take a scenic cruise on the Danube River.', '2023-09-02', '2023-09-09', 1543.00, '073bf718-4d8e-4c85-99de-fe045c3d665e.jpg'),
-(2, 'Rome', 'Immerse yourself in the rich history of Rome, the \"Eternal City.\" Marvel at the Colosseum and ancient ruins, toss a coin in the Trevi Fountain, and savor the delicious Italian cuisine.', '2023-11-12', '2023-11-19', 1879.00, 'b8acfe6b-b7cc-41fc-a6d0-8fb5620a3168.jpg'),
-(3, 'Florence', 'Discover the art and culture of Florence, the birthplace of the Renaissance. Visit the Uffizi Gallery, climb to the top of the Duomo, and indulge in a traditional Tuscan meal.', '2023-05-04', '2023-05-07', 1522.00, 'c5b57076-f46d-4644-a718-a88ca5b24afb.jpg'),
-(4, 'Venice', 'Explore the magical city of Venice, built on a network of canals. Take a gondola ride, visit the stunning St. Mark\'s Basilica, and sample the local seafood.', '2023-08-15', '2023-08-22', 2350.00, 'f036075a-b620-46ad-aca6-56bcfff2d74a.jpg'),
-(5, 'Buenos Aires', 'Experience the vibrant culture of Buenos Aires. Tango the night away, stroll through colorful neighborhoods, and savor the delicious Argentine cuisine.', '2023-12-19', '2023-12-29', 3525.00, '06f0ceb2-82dd-4c51-900e-3c102efcf87e.jpg'),
-(6, 'London', 'Discover the historic landmarks and modern culture of London. Visit the Tower of London, take a ride on the London Eye, and enjoy a traditional afternoon tea.', '2023-07-10', '2023-07-15', 2200.00, '2397a82a-78c0-4c81-85d0-12b9f49b2352 .jpg'),
-(7, 'Huacachina', 'Escape to the tranquil oasis of Huacachina, surrounded by sand dunes. Go sandboarding, take a relaxing spa day, and watch the sunset over the desert.', '2023-11-13', '2023-11-19', 3600.00, '21a09e93-a316-4b63-947f-ec44156049f4.jpg'),
-(8, 'Madrid', 'Experience the lively spirit of Madrid, the capital of Spain. Admire the art at the Prado Museum, taste the delicious tapas, and dance the night away at a flamenco show.', '2023-06-14', '2023-06-26', 2960.00, '115d9361-ffca-4000-9b0f-3b5f57df5773.jpg'),
-(9, 'Norway', 'Embark on a breathtaking journey through the natural beauty of Norway. Cruise through the fjords, hike through the mountains, and see the Northern Lights.', '2023-10-10', '2023-10-17', 3756.00, '3c6220cc-6a80-434d-b4b5-9b7b6f237d14 .jpg'),
-(10, 'Paris', 'Fall in love with the enchanting city of Paris, known as the \"City of Love.\" Visit the Eiffel Tower, stroll along the Champs-Élysées, and indulge in the delicious French cuisine.', '2023-08-01', '2023-08-06', 2009.00, '9bbfde53-1e29-4677-a9af-7b34d19994de.jpg'),
-(11, 'Tbilisi', 'Discover the unique culture and history of Tbilisi, the capital of Georgia. Visit the Narikala Fortress, taste the traditional Georgian cuisine, and relax in the sulfur baths.', '2024-04-16', '2024-04-21', 2789.00, '7870c267-b882-4df1-be4e-f2024c0881b7.jpg'),
-(12, 'Tokyo', 'Experience the dynamic energy of Tokyo, the bustling capital of Japan. Explore the futuristic architecture, indulge in the delicious sushi, and immerse yourself in the vibrant nightlife.', '2023-09-03', '2023-09-13', 5260.00, '34e24f74-7bfa-4c7f-bff0-40c3cd487663.jpg'),
-(13, 'SIT Office', 'The real meaning of progress', '2023-05-17', '2023-05-25', 2500.00, '2f963e53-65db-476b-a93a-6eea7cb5c763.jpg');
+(1, 'Budapest', 'Experience the beauty of Budapest, known as the \"Paris of the East.\" Admire the stunning architecture, soak in the thermal baths, and take a scenic cruise on the Danube River.', '2023-09-02', '2023-09-09', '1543.00', '073bf718-4d8e-4c85-99de-fe045c3d665e.jpg'),
+(2, 'Rome', 'Immerse yourself in the rich history of Rome, the \"Eternal City.\" Marvel at the Colosseum and ancient ruins, toss a coin in the Trevi Fountain, and savor the delicious Italian cuisine.', '2023-05-19', '2023-05-30', '1879.00', 'b8acfe6b-b7cc-41fc-a6d0-8fb5620a3168.jpg'),
+(3, 'Florence', 'Discover the art and culture of Florence, the birthplace of the Renaissance. Visit the Uffizi Gallery, climb to the top of the Duomo, and indulge in a traditional Tuscan meal.', '2023-05-04', '2023-05-07', '1522.00', 'c5b57076-f46d-4644-a718-a88ca5b24afb.jpg'),
+(4, 'Venice', 'Explore the magical city of Venice, built on a network of canals. Take a gondola ride, visit the stunning St. Mark\'s Basilica, and sample the local seafood.', '2023-08-15', '2023-08-22', '2350.00', 'f036075a-b620-46ad-aca6-56bcfff2d74a.jpg'),
+(5, 'Buenos Aires', 'Experience the vibrant culture of Buenos Aires. Tango the night away, stroll through colorful neighborhoods, and savor the delicious Argentine cuisine.', '2023-12-19', '2023-12-29', '3525.00', '06f0ceb2-82dd-4c51-900e-3c102efcf87e.jpg'),
+(6, 'London', 'Discover the historic landmarks and modern culture of London. Visit the Tower of London, take a ride on the London Eye, and enjoy a traditional afternoon tea.', '2023-05-28', '2023-06-04', '2200.00', '2397a82a-78c0-4c81-85d0-12b9f49b2352 .jpg'),
+(7, 'Huacachina', 'Escape to the tranquil oasis of Huacachina, surrounded by sand dunes. Go sandboarding, take a relaxing spa day, and watch the sunset over the desert.', '2023-11-13', '2023-11-19', '3600.00', '21a09e93-a316-4b63-947f-ec44156049f4.jpg'),
+(8, 'Madrid', 'Experience the lively spirit of Madrid, the capital of Spain. Admire the art at the Prado Museum, taste the delicious tapas, and dance the night away at a flamenco show.', '2023-06-14', '2023-06-26', '2960.00', '115d9361-ffca-4000-9b0f-3b5f57df5773.jpg'),
+(9, 'Norway', 'Embark on a breathtaking journey through the natural beauty of Norway. Cruise through the fjords, hike through the mountains, and see the Northern Lights.', '2023-10-10', '2023-10-17', '3756.00', '3c6220cc-6a80-434d-b4b5-9b7b6f237d14 .jpg'),
+(10, 'Paris', 'Fall in love with the enchanting city of Paris, known as the \"City of Love.\" Visit the Eiffel Tower, stroll along the Champs-Élysées, and indulge in the delicious French cuisine.', '2023-08-01', '2023-08-06', '2009.00', '9bbfde53-1e29-4677-a9af-7b34d19994de.jpg'),
+(11, 'Tbilisi', 'Discover the unique culture and history of Tbilisi, the capital of Georgia. Visit the Narikala Fortress, taste the traditional Georgian cuisine, and relax in the sulfur baths.', '2023-05-24', '2023-06-04', '2789.00', '7870c267-b882-4df1-be4e-f2024c0881b7.jpg'),
+(12, 'Tokyo', 'Experience the dynamic energy of Tokyo, the bustling capital of Japan. Explore the futuristic architecture, indulge in the delicious sushi, and immerse yourself in the vibrant nightlife.', '2023-09-03', '2023-09-13', '5260.00', '34e24f74-7bfa-4c7f-bff0-40c3cd487663.jpg');
 
 --
 -- Indexes for dumped tables
@@ -125,13 +163,13 @@ ALTER TABLE `vacations`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `vacations`
 --
 ALTER TABLE `vacations`
-  MODIFY `vacationsId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `vacationsId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- Constraints for dumped tables
